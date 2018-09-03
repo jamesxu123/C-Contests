f117172be20c0dfea3b03f18377a370262af9101
James Xu <xujames007@gmail.com>
2018-09-02 20:59:53 -0600

    Initial Commit

---
+++ Cpp Contests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp Contests
+//
+//  Created by James Xu on 2018-09-02.
+//  Copyright © 2018 James Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
